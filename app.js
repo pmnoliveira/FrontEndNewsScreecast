c3bb738ad48ca96c08169aa8da79551e5193eb71
c0ffeebreak <pmnoliveira09@gmail.com>
2016-11-29 22:12:12 +0000

    Animation Added JS

--- app.js
+++ app.js
@@ -5,7 +5,9 @@
 $(document).ready(function(){
   $(".js-like").on("click", function(event){
     event.preventDefault();
-    
-    $(this).text("Liked!");
+
+    $(this).text("Liked!")
+      .closest(".news-item")
+      .addClass("is-liked");
   });
 });
